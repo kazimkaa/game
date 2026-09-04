6a3aedd1da6b76b2dd7e2d4a3f330608383a5372
kazimkaa <150304370+kazimkaa@users.noreply.github.com>
2026-09-04 15:27:30 +0300

    Update index.js

--- sync/index.js
+++ sync/index.js
@@ -3,6 +3,7 @@ const PlayerSync = require('./playerSync');
 const BotSync = require('./botSync');
 const CreepSync = require('./creepSync');
 const AnimationSync = require('./animationSync');
+const { send } = require('../utils/helpers');  // ← ДОБАВИТЬ ЭТУ СТРОКУ
 
 class SyncManager {
     constructor(wss, players, state) {
@@ -18,53 +19,32 @@ class SyncManager {
         this.isRunning = false;
     }
 
-    /**
-     * Запускает все синхронизации
-     */
     startAll() {
         if (this.isRunning) return;
-        
         this.isRunning = true;
         console.log('[SyncManager] 🚀 Запуск всех синхронизаций');
-        
         this.playerSync.start(100);
         this.botSync.start();
         this.creepSync.start();
         this.animationSync.start();
     }
 
-    /**
-     * Останавливает все синхронизации
-     */
     stopAll() {
         this.isRunning = false;
         console.log('[SyncManager] ⏹ Остановка всех синхронизаций');
-        
         this.playerSync.stop();
         this.botSync.stop();
         this.creepSync.stop();
         this.animationSync.stop();
     }
 
-    /**
-     * Быстрая синхронизация всех данных для нового игрока
-     */
     syncAllToClient(ws) {
-        // Синхронизация игроков
         this.playerSync.syncToClient(ws);
-        
-        // Синхронизация ботов
         this.botSync.syncToClient(ws);
-        
-        // Синхронизация крипов
         this.creepSync.syncToClient(ws);
-        
         console.log('[SyncManager] 📤 Отправлена полная синхронизация клиенту');
     }
 
-    /**
-     * Принудительная полная синхронизация всех данных всем
-     */
     syncAllToAll() {
         this.playerSync.syncAll();
         this.botSync.syncAll();
